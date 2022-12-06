b277bb65710eb2c32cc6e0d181d51dcfc2f49667
whj <2531161998@qq.com>
2022-12-06 10:08:29 +0800

    flx：布局更新

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,8 +13,16 @@
       </div>
     </div>
     <div class="home-content">
-      <div class="content-box"></div>
-      <textarea class="content-inp"></textarea>
+      <div class="content-box">
+        <div class="content-title">测试用户</div>
+        <div class="chat-box">
+          <div></div>
+        </div>
+      </div>
+      <div class="content-inp-box">
+        <textarea class="inp"></textarea>
+        <div class="btn">发送</div>
+      </div>
     </div>
   </div>
 </template>
@@ -31,12 +39,12 @@ export default {
   width: 900px;
   height: 680px;
   margin: 100px auto;
-  border: 1px solid #ccc;
+  border: 1px solid #e7e7e7;
   .home-sidebar {
     width: 240px;
     background: #ebe8e6;
     height: 100%;
-    border-right: 1px solid #ccc;
+    border-right: 1px solid #e7e7e7;
     overflow: auto;
   }
   .home-content {
@@ -67,9 +75,44 @@ export default {
   .content-box {
     height: 450px;
   }
-  .content-inp {
+  .content-inp-box {
+    position: relative;
+    border-top: 1px solid #e7e7e7;
     width: 99%;
     height: 220px;
+    .inp {
+      margin-top: 20px;
+      height: 70%;
+      width: 92%;
+      margin-left: 20px;
+    }
+    textarea {
+      border: none;
+      outline: none; /*边线不显示*/
+      resize: none; /*禁止拉伸*/
+      background: #fff; /*带点绿*/
+      appearance: none;
+    }
+    .btn {
+      width: 86px;
+      height: 34px;
+      right: 20px;
+      border-radius: 4px;
+      bottom: 0px;
+      font-size: 14px;
+      line-height: 34px;
+      background-color: #e9e9e9;
+      text-align: center;
+      position: absolute;
+    }
+  }
+  .content-title {
+    display: flex;
+    padding: 10px;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e7e7e7;
+    font-size: 20px;
   }
 }
 </style>
